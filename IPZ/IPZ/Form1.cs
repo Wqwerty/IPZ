7f81dd11de378726d97b4fc8ea205167c696622e
Łukasz Rdzeń <byk777@gmail.com>
2014-10-13 12:35:05 +0200

    działa?

--- IPZ/IPZ/Form1.cs
+++ IPZ/IPZ/Form1.cs
@@ -16,5 +16,10 @@ namespace IPZ
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Działa!");
+        }
     }
 }
